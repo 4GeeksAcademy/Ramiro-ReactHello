b217389ca34b9d2a1f3324e94b646a5c2b4f4eb6
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-02-01 20:29:15 +0000

    Creacion en conjunto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Navbar from "./navbar";
+import { MyFooter } from "./footer";
+import { Jumbotron } from "./jumbotron";
+import Card from "./card";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -7,7 +10,21 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 	return (
-		<Navbar/>
+		<div>
+			<Navbar />
+			<div class="container">
+				<div className="row">
+				<Jumbotron />
+				</div>
+				<div class="row p-5 ">
+					<div class="col-lg-2 col-md-5 col-sm-11 m-2"><Card/></div>
+					<div class="col-lg-2 col-md-5 col-sm-11 m-2"><Card/></div>
+					<div class="col-lg-2 col-md-5 col-sm-11 m-2"><Card/></div>
+					<div class="col-lg-2 col-md-5 col-sm-11 m-2"><Card/></div>
+				</div>
+			</div>
+			<MyFooter />
+		</div>
 	);
 };
 
