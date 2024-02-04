1698edaaa8a35679d9b8f3de626a16288ed9e644
Ramiro Scarinci <151871077+RamiroSca@users.noreply.github.com>
2024-02-04 10:00:55 +0000

    Correccion del Navbar. Proyecto Terminadogit add .git add .

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -2,26 +2,38 @@ import React from "react";
 
 function Navbar() {
     return (
-        <nav className="bg-dark p-2 px-5 ">
-            <div className="container navbar navbar-expand-lg navbar-light bg-dark p-2 px-5">
-                <div className="col-2"></div>
-                <div className="col-8">
-                    <a className="navbar-brand text-white" href="#">Start Boostrap</a>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                        <span className="navbar-toggler-icon"></span>
-                    </button>
-                    <div className="collapse navbar-collapse flex-row-reverse" id="navbarNavAltMarkup">
-                        <div className="navbar-nav">
-                            <a className="nav-link active text-white" href="#">Home <span className="sr-only">(current)</span></a>
-                            <a className="nav-link text-white-50">About</a>
-                            <a className="nav-link text-white-50">Services</a>
-                            <a className="nav-link text-white-50">Contact</a>
-                        </div>
+        <div className="bg-dark">
+            <div className="container">
+                <div className="row">
+                    <div className="col-2"></div>
+                    <div className="col-8">
+                        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+                            <a className="navbar-brand text-white" href="#">Start Booststrap</a>
+                            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                                <span className="navbar-toggler-icon"></span>
+                            </button>
+                            <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+                                <ul className="navbar-nav">
+                                    <li className="nav-item active">
+                                        <a className="nav-link text-white">Home <span className="sr-only">(current)</span></a>
+                                    </li>
+                                    <li className="nav-item ">
+                                        <a className="nav-link text-white-50">Features</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link text-white-50">Pricing</a>
+                                    </li>
+                                    <li className="nav-item">
+                                        <a className="nav-link text-white-50">Disabled</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
                     </div>
+                    <div className="col-2"></div>
                 </div>
-                <div className="col-3"></div>
-                </div>
-        </nav>
+            </div>
+        </div>
     )
 }
 
